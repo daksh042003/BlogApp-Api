66bae8257565289cecf48691cda81b9d491e75bc
Daksh Pandey <dakshpandey127@gmail.com>
2023-08-28 13:33:10 +0530

    changes

---
+++ routes/blog.js
@@ -0,0 +1,8 @@
+const express =require("express");
+const router = express.Router();
+
+const {createComment} = require("../controllers/commentController");
+
+router.post("/comments/create",createComment);
+
+module.exports = router;
