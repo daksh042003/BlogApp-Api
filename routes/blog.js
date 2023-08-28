755cf3f0a990ccaf40dfdbfb1fded32d2c38adeb
Daksh Pandey <dakshpandey127@gmail.com>
2023-08-28 14:51:57 +0530

    cahnges

--- routes/blog.js
+++ routes/blog.js
@@ -2,7 +2,10 @@ const express =require("express");
 const router = express.Router();
 
 const {createComment} = require("../controllers/commentController");
+const { createPost } = require("../controllers/PostController");
 
 router.post("/comments/create",createComment);
+router.post("/post/create",createPost);
+
 
 module.exports = router;
